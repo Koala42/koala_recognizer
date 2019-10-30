c882af2147ba0381802e5dd943c17868ee69726c
Tauronek <39222209+FilipK268@users.noreply.github.com>
2019-10-30 09:26:56 +0100

    final

--- src/dropzone/Dropzone.jsx
+++ src/dropzone/Dropzone.jsx
@@ -5,6 +5,7 @@ import imageRecognition from "./ImageRecognition";
 export default function Dropzone() {
   const [path, setPath] = useState("");
   const [text, setText] = useState("");
+  const [margin, setMargin] = useState("100px");
   const openFile = input => {
     const reader = new FileReader();
     reader.onload = () => {
@@ -25,6 +26,7 @@ export default function Dropzone() {
     };
     img.src = url;
     setText("Loading...");
+    setMargin("0px");
     let text = await imageRecognition(img);
     setText(text);
   }, []);
@@ -32,26 +34,25 @@ export default function Dropzone() {
 
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Is it a Koala?</h1>
-      <div className={styles.container}>
-        <div className={styles.dropzone} {...getRootProps()}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <p>Drop the files here ...</p>
-          ) : (
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          )}
-        </div>
-        <div className={styles.image}>
-          <img
-            id="img"
-            src={path}
-            crossOrigin="true"
-            style={{ marginBottom: "20px", borderRadius: "12px" }}
-            height="250"
-            alt=""
-          />
-          <div id="result">{text}</div>
+      <div style={{ marginTop: margin }}>
+        <h1 style={{ textAlign: "center" }}>Is it a Koala?</h1>
+        <div className={styles.container}>
+          <div className={styles.dropzone} {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            )}
+          </div>
+
+          <div>
+            <img id="img" src={path} crossOrigin="true" class={styles.image} alt="" />
+          </div>
+          <div id="result" class={styles.result}>
+            {text}
+          </div>
+          {/* <div className={styles.image} style={{ backgroundImage: `url(${path})` }}> */}
         </div>
       </div>
     </div>
